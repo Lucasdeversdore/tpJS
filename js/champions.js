8d31e35298cb87b21c8bb9b7f4c15a5a0f849f3a
Tony <tony.kerguen@etu.univ-orleans.fr>
2025-04-01 15:53:29 +0200

    champion fav

--- js/champions.js
+++ js/champions.js
@@ -11,11 +11,27 @@ class Champions {
             const response = await fetch(CONFIG.championsUrl);
             const data = await response.json();
             this.championsData = Object.values(data);
+            this.loadFavorites();
             this.render();
             this.addSearchListener();
         } catch (error) {
             console.error("Erreur lors du chargement des champions :", error);
         }
+    }
+
+    loadFavorites() {
+        const favorites = JSON.parse(localStorage.getItem("championFavorites")) || {};
+        this.championsData.forEach(champion => {
+            champion.favorite = !!favorites[champion.id];
+        });
+    }
+
+    toggleFavorite(championId) {
+        const favorites = JSON.parse(localStorage.getItem("championFavorites")) || {};
+        favorites[championId] = !favorites[championId];
+        localStorage.setItem("championFavorites", JSON.stringify(favorites));
+        this.loadFavorites();
+        this.renderList(this.championsData);
     }
 
     render() {
@@ -25,7 +41,6 @@ class Champions {
             <input type="text" id="search-champions" placeholder="Rechercher un champion" />
             <ul id="champions-list"></ul>
         `;
-
         this.renderList(this.championsData);
     }
 
@@ -33,18 +48,30 @@ class Champions {
         const list = document.getElementById("champions-list");
         list.innerHTML = "";
 
+        champions.sort((a, b) => (b.favorite ? 1 : 0) - (a.favorite ? 1 : 0));
+
         champions.forEach(champion => {
             const div = document.createElement("div");
             div.className = "champion";
             div.innerHTML = `
+                <input type="checkbox" id="fav-${champion.id}" ${champion.favorite ? "checked" : ""} />
+                <label for="fav-${champion.id}">★</label>
                 <img src="https://ddragon.leagueoflegends.com/cdn/img/champion/loading/${champion.id}_0.jpg" alt="${champion.name}">
                 <h2>${champion.name}</h2>
                 <p>${champion.title}</p>
             `;
-            div.addEventListener("click", () => {
-                history.pushState({}, "", `/champion/${champion.id}`);
-                afficherChampionDetails(champion.id);
+
+            div.querySelector("input").addEventListener("change", () => {
+                this.toggleFavorite(champion.id);
             });
+
+            div.addEventListener("click", (e) => {
+                if (e.target.tagName !== "INPUT" && e.target.tagName !== "LABEL") {
+                    history.pushState({}, "", `/champion/${champion.id}`);
+                    afficherChampionDetails(champion.id);
+                }
+            });
+
             list.appendChild(div);
         });
     }
